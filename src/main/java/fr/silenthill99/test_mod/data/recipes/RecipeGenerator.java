13730c05586c9a876b84d3c5fb1491dff31004c7
silenthill99 <florian.graziani@sfr.fr>
2024-09-13 17:57:32 +0200

    Réécriture du Data generator des crafts

--- src/main/java/fr/silenthill99/test_mod/data/recipes/RecipeGenerator.java
+++ src/main/java/fr/silenthill99/test_mod/data/recipes/RecipeGenerator.java
@@ -1,11 +1,14 @@
 package fr.silenthill99.test_mod.data.recipes;
 
+import fr.silenthill99.test_mod.Main;
 import fr.silenthill99.test_mod.init.ModBlocks;
 import fr.silenthill99.test_mod.init.ModItems;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.ItemLike;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 import org.jetbrains.annotations.NotNull;
@@ -39,10 +42,66 @@ public class RecipeGenerator extends RecipeProvider implements IConditionBuilder
                 .unlockedBy("unlock", InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.BISMUTH))
                 .save(recipeOutput);
 
-        oreSmelting(recipeOutput, BISMUTH_SMELTABLES, RecipeCategory.MISC, ModItems.BISMUTH.get(), 0.25f, 200,
-                "bismuth");
-        oreBlasting(recipeOutput, BISMUTH_SMELTABLES, RecipeCategory.MISC, ModItems.BISMUTH.get(), 0.25f, 100,
-                "bismuth");
+        oreSmelting(recipeOutput, BISMUTH_SMELTABLES, RecipeCategory.MISC, ModItems.BISMUTH.get(), 0.25f,
+                200, "bismuth");
+        oreBlasting(recipeOutput, BISMUTH_SMELTABLES, RecipeCategory.MISC, ModItems.BISMUTH.get(), 0.25f,
+                100, "bismuth");
 
+    }
+
+    protected static void oreSmelting(
+            @NotNull RecipeOutput recipeOutput, List<ItemLike> ingredients, @NotNull RecipeCategory category,
+            @NotNull ItemLike result, float experience, int cookingTime, @NotNull String group
+    ) {
+        oreCooking(
+                recipeOutput,
+                RecipeSerializer.SMELTING_RECIPE,
+                SmeltingRecipe::new,
+                ingredients,
+                category,
+                result,
+                experience,
+                cookingTime,
+                group,
+                "_from_smelting"
+        );
+    }
+
+    protected static void oreBlasting(
+            @NotNull RecipeOutput recipeOutput, List<ItemLike> ingredients, @NotNull RecipeCategory category,
+            @NotNull ItemLike result, float experience, int cookingTime, @NotNull String group
+    ) {
+        oreCooking(
+                recipeOutput,
+                RecipeSerializer.BLASTING_RECIPE,
+                BlastingRecipe::new,
+                ingredients,
+                category,
+                result,
+                experience,
+                cookingTime,
+                group,
+                "_from_blasting"
+        );
+    }
+
+    protected static <T extends AbstractCookingRecipe> void oreCooking(
+            @NotNull RecipeOutput recipeOutput,
+            RecipeSerializer<T> serializer,
+            AbstractCookingRecipe.@NotNull Factory<T> recipeFactory,
+            List<ItemLike> ingredients,
+            @NotNull RecipeCategory category,
+            @NotNull ItemLike result,
+            float experience,
+            int cookingTime,
+            @NotNull String group,
+            String suffix
+    ) {
+        for (ItemLike itemlike : ingredients) {
+            SimpleCookingRecipeBuilder.generic(Ingredient.of(itemlike), category, result, experience, cookingTime, serializer, recipeFactory)
+                    .group(group)
+                    .unlockedBy(getHasName(itemlike), has(itemlike))
+                    .save(recipeOutput, ResourceLocation.fromNamespaceAndPath(Main.MODID, getItemName(result) + suffix + "_" + getItemName(itemlike)));
+        }
     }
 }
